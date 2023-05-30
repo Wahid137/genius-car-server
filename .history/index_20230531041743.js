3fec2475a21c22095f1fca3c68ebb2341c2f5426
wahid-ahmed <wahidahmedshanto@gmail.com>
2023-05-31 04:29:33 +0600

    intial setup done and insert services data

---
+++ .history/index_20230531041743.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+require('dotenv').config()
+
+// middle wares
+app.use(cors())
+app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gfg0jvx.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        const serviceCollection = client.db("geniusCarService").collection("servicesList");
+
+    } finally {
+
+
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send("genius car is running")
+})
+
+app.listen(port, () => {
+    console.log(`Genius car server is running on ${port}`)
+})
